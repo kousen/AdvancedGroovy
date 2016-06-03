033ee873d13f438f2dc14472977eb81a87065f53
Kenneth Kousen <ken.kousen@kousenit.com>
2016-06-03 14:31:34 +0200

    Rename package for ICNDB example

--- src/main/groovy/icndb/icndb_script_only.groovy
+++ src/main/groovy/icndb/icndb_script_only.groovy
@@ -1,4 +1,4 @@
-package builder
+package icndb
 
 import groovy.json.JsonSlurper
 
--- src/main/groovy/icndb/icndb_with_textarea_and_button.groovy
+++ src/main/groovy/icndb/icndb_with_textarea_and_button.groovy
@@ -1,4 +1,4 @@
-package builder
+package icndb
 
 import groovy.json.JsonSlurper
 import groovy.swing.SwingBuilder
