64f765dd3eaf259dbf58f54814fc34cc1da8a6b0
Kenneth Kousen <ken.kousen@kousenit.com>
2016-06-03 15:33:18 +0200

    Remove obsolete copyright statements

--- src/main/groovy/range/Pojo.groovy
+++
@@ -1,9 +0,0 @@
-package range;
-
-import groovy.transform.Immutable;
-
-@Immutable
-class Pojo {
-    int a;
-    String b;
-}
--- src/main/groovy/range/use_demo.groovy
+++
@@ -1,7 +0,0 @@
-package range
-
-Pojo p = new Pojo(a:3, b:'hello')
-println p
-
-p.a = 5
-println p
