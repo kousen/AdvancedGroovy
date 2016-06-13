afeafcc6ac37dc9758d80e94de4764733d1733d3
Kenneth Kousen <ken.kousen@kousenit.com>
2016-06-13 10:13:54 -0400

    Fix typo in URL

--- src/main/groovy/openweather/parse_weather.groovy
+++ src/main/groovy/openweather/parse_weather.groovy
@@ -6,8 +6,8 @@ import com.google.gson.Gson
 
 String appid = new File('api_key.txt').text
 
-def url = "http://api.openweathermap.org/data/2.5/weather?APPID=$appid&q=cambridge,ma"
+def url = "http://api.openweathermap.org/data/2.5/weather?q=copenhagen,denmark&APPID=$appid"
 def jsonTxt = url.toURL().text
 println JsonOutput.prettyPrint(jsonTxt)
-Gson gson = new Gson()
-println gson.fromJson(jsonTxt, Model)
+//Gson gson = new Gson()
+//println gson.fromJson(jsonTxt, Model)
